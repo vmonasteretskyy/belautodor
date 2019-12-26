8b55a755da6baa5b931f4c8ffe54ad3cf5dbe907
v.monasteretskyy <v.monasteretskyy@gmail.com>
2019-12-26 11:49:16 +0200

    line animations

--- js/common.js
+++ js/common.js
@@ -118,6 +118,32 @@ $(document).ready(function () {
     });
 
 
+    function isScrolledIntoView(elem) {
+        var docViewTop = $(window).scrollTop();
+        var docViewBottom = docViewTop + $(window).height();
+
+        var elemTop = $(elem).offset().top;
+        var elemBottom = elemTop + $(elem).height();
+
+        return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+    }
+
+    $(window).scroll(function () {
+        $('.company__linetexts').each(function () {
+            if (isScrolledIntoView(this) === true) {
+                $(this).addClass('visible');
+            }
+        });
+        $('.about__linetexts').each(function () {
+            if (isScrolledIntoView(this) === true) {
+                $(this).addClass('visible');
+            }
+        });
+    });
+
+
+
+
 
 });
 
